71706b7667fd38ba7020a1107091e7796a617797
SashaAllan <sashaabdsalamallan@gmail.com>
2023-08-05 00:02:43 +0300

    Second Commit

---
+++ function.py
@@ -0,0 +1,19 @@
+x=int(input("the first number"))
+y=int(input(" the second number"))
+def add_num(x,y):
+
+    return x+y
+def sub_num(x,y):
+
+    return x-y
+def mult_num(x,y):
+
+    return x*y
+def div_num(x,y):
+
+    return x/y
+
+print("add numb :",add_num(x,y))
+print("sub num :",sub_num(x,y))
+print("mult num :",mult_num(x,y))
+print("div num :",div_num(x,y))
