9b542c013d50490662fd513ea3db69304aaa4bce
SashaAllan <sashaabdsalamallan@gmail.com>
2023-08-04 22:15:44 +0300

    Second Commit

---
+++ Task4.py
@@ -0,0 +1,8 @@
+Name=str(input('Enter the fullName'))
+numb=str(input('Enter the mobile number'))
+birth=int(input('Enter the year of birthday'))
+userAge=2023-birth
+num=numb.split("-")
+print(Name)
+print(userAge)
+print(num)
