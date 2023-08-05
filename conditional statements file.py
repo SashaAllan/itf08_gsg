6fe0b59fd4c976c022d1cbaf592c14ff6ce97723
SashaAllan <sashaabdsalamallan@gmail.com>
2023-08-05 19:51:15 +0300

    Second Commit

---
+++ conditional statements file.py
@@ -0,0 +1,28 @@
+def area_triangle(base,height):
+    area =.5*base*height
+
+
+    check_area(area)
+def area_rectangular(height,width):
+    area= height*width
+
+    check_area(area)
+
+def area_circle(radius):
+    area= 3.14*(radius**2)
+
+    check_area(area)
+def check_area(area):
+    print(f"Area is {area}")
+    if area>=10:
+     print("area is bigger than 10")
+
+    elif 10 > area > 0:
+     print("area is smaller than 10")
+
+    else:print("invalid area")
+
+
+area_rectangular(10,2)
+area_circle(-1)
+area_triangle(5,5)
---
+++ task5.py
@@ -0,0 +1,18 @@
+
+
+
+def area_triangle(base,height):
+    area =.5*base*height
+def area_rectangular(height,width):
+    area= height*width
+def area_circle(radius):
+    area= 3.14*(radius**2)
+
+
+
+
+ar=area_triangle(5,6)
+print(ar)
+am=area_circle(0.2)
+print(am)
+
