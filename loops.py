e599133c052a12f6b2f77449ae2ac9fe95f8380c
SashaAllan <sashaabdsalamallan@gmail.com>
2023-08-09 10:59:37 +0300

    Second Commit

--- loops.py
+++ loops.py
@@ -1,4 +1,3 @@
-
 def sum ():
     num1 = int(input("the first number"))
     num2 = int(input(" the second number"))
@@ -14,15 +13,12 @@ def multiply():
     num2 = int(input(" the second number"))
     print("The Multiply is =", num1 * num2)
     print("*************************")
-
 def devision():
     num1 = int(input("the first number"))
     num2 = int(input(" the second number"))
     print("The Devision is =", num1 / num2)
     print("*************************")
-
 def triangle_area():
-
     base = int(input("enter the base triangle area"))
     length = int(input("enter the length triangle area"))
     print("The triangle area is = ",.5*base*length)
@@ -38,7 +34,6 @@ def rectangle_area():
     print("*************************")
 
 while True:
-
      menu = int(input("1. Sum\n" 
          "2.Substract\n" 
          "3.Multiply\n" 
@@ -52,7 +47,6 @@ while True:
         break
        else:
         menu = int(input("Invalid input,Enter valid Selction "))
-
      if menu == 1:
         sum()
      elif menu == 2:
@@ -63,11 +57,9 @@ while True:
        devision()
      elif menu == 5:
        triangle_area()
-
      elif menu == 6:
        circle_area()
      elif menu == 7:
        rectangle_area()
-
      elif menu == 8:
         break
